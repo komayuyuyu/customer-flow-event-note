f0a5c9b060eaa093d485eb35576288dd7f5e4946
komayuyuyu <komayek@gmail.com>
2026-07-01 06:14:55 +0900

    本人限定の記録権限を反映

--- firebase-config.js
+++ firebase-config.js
@@ -6,5 +6,5 @@ window.CUSTOMER_FLOW_FIREBASE_CONFIG = {
     "projectId": "customer-flow-event-note",
     "appId": "1:766604675895:web:04039db41b5244f71a68b2"
   },
-  "allowedUid": ""
+  "allowedUid": "oOPQbgmbcEVAfPKJpkUgZuZNux02"
 };
